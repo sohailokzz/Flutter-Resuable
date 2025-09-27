ca3b2e1fc97329107c553e1dc1d70fa133f12090
Muhammad Sohail <59048193+sohailokzz@users.noreply.github.com>
2025-09-27 14:47:18 +0500

    Bug Fix
    Bug Fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_reusable/screens/auth_screens/login_screen.dart';
 import 'package:flutter_reusable/screens/auth_screens/login_screen_vm.dart';
-import 'package:flutter_reusable/screens/home_screen.dart';
 import 'package:provider/provider.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import 'theme/app_theme.dart';
