cf48b71cde0c3dff8e533413d52f6be14f24558a
Muhammad Sohail <59048193+sohailokzz@users.noreply.github.com>
2025-10-16 23:34:53 +0500

    custom rich text
    custom rich text

--- lib/screens/auth_screens/login_screen.dart
+++ lib/screens/auth_screens/login_screen.dart
@@ -4,6 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import '../../utilis/app_colors.dart';
 import '../../utilis/image_assets.dart';
+import '../../widgets/custom_richtext.dart';
 import 'login_screen_vm.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -420,47 +421,6 @@ class DividerRow extends StatelessWidget {
           ),
         ),
       ],
-    );
-  }
-}
-
-class CustomRichText extends StatelessWidget {
-  const CustomRichText({
-    super.key,
-    required this.title,
-    required this.subtitle,
-    required this.onTab,
-    required this.subtitleTextStyle,
-  });
-  final String title, subtitle;
-  final TextStyle subtitleTextStyle;
-  final VoidCallback onTab;
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTab,
-      child: RichText(
-        textAlign: TextAlign.center,
-        text: TextSpan(
-          text: title,
-          style:
-              GoogleFonts.plusJakartaSans(
-                fontSize: 14,
-                fontWeight: FontWeight.w500,
-                color: AppColor.kWhite,
-              ).copyWith(
-                color: AppColor.kGrayscale40,
-                fontWeight: FontWeight.w600,
-                fontSize: 14,
-              ),
-          children: <TextSpan>[
-            TextSpan(
-              text: subtitle,
-              style: subtitleTextStyle,
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ lib/widgets/custom_richtext.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../utilis/app_colors.dart';
+
+class CustomRichText extends StatelessWidget {
+  const CustomRichText({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.onTab,
+    required this.subtitleTextStyle,
+  });
+  final String title, subtitle;
+  final TextStyle subtitleTextStyle;
+  final VoidCallback onTab;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTab,
+      child: RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+          text: title,
+          style:
+              GoogleFonts.plusJakartaSans(
+                fontSize: 14,
+                fontWeight: FontWeight.w500,
+                color: AppColor.kWhite,
+              ).copyWith(
+                color: AppColor.kGrayscale40,
+                fontWeight: FontWeight.w600,
+                fontSize: 14,
+              ),
+          children: <TextSpan>[
+            TextSpan(
+              text: subtitle,
+              style: subtitleTextStyle,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
