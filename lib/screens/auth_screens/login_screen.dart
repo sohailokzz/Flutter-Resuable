ddde97dbbe2a9fba789b3070aa009149f6e270f3
Muhammad Sohail <59048193+sohailokzz@users.noreply.github.com>
2025-10-27 23:20:28 +0500

    updated
    updated

--- lib/screens/auth_screens/login_screen.dart
+++ lib/screens/auth_screens/login_screen.dart
@@ -9,6 +9,7 @@ import '../../utilis/app_colors.dart';
 import '../../utilis/image_assets.dart';
 import '../../widgets/custom_richtext.dart';
 import '../../widgets/password_textfield.dart';
+import '../../widgets/primary_button.dart';
 import '../../widgets/terms_privacy_text.dart';
 import 'login_screen_vm.dart';
 
@@ -268,105 +269,3 @@ class _SecondaryButtonState extends State<SecondaryButton>
     );
   }
 }
-
-class PrimaryButton extends StatefulWidget {
-  final VoidCallback onTap;
-  final String text;
-  final double? width;
-  final double? height;
-  final double? borderRadius, elevation;
-  final double? fontSize;
-  final IconData? iconData;
-  final Color? textColor, bgColor;
-  const PrimaryButton({
-    super.key,
-    required this.onTap,
-    required this.text,
-    this.width,
-    this.height,
-    this.elevation = 5,
-    this.borderRadius,
-    this.fontSize,
-    required this.textColor,
-    required this.bgColor,
-    this.iconData,
-  });
-
-  @override
-  State<PrimaryButton> createState() => _PrimaryButtonState();
-}
-
-class _PrimaryButtonState extends State<PrimaryButton>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  final Duration _animationDuration = const Duration(milliseconds: 300);
-  final Tween<double> _tween = Tween<double>(begin: 1.0, end: 0.95);
-  @override
-  void initState() {
-    _controller =
-        AnimationController(
-          vsync: this,
-          duration: _animationDuration,
-        )..addListener(() {
-          setState(() {});
-        });
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        _controller.forward().then((_) {
-          _controller.reverse();
-        });
-        widget.onTap();
-      },
-      child: ScaleTransition(
-        scale: _tween.animate(
-          CurvedAnimation(
-            parent: _controller,
-            curve: Curves.easeOut,
-            reverseCurve: Curves.easeIn,
-          ),
-        ),
-        child: Card(
-          elevation: widget.elevation ?? 5,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(widget.borderRadius!),
-          ),
-          child: Container(
-            height: widget.height ?? 55,
-            alignment: Alignment.center,
-            width: widget.width ?? double.maxFinite,
-            decoration: BoxDecoration(
-              color: widget.bgColor,
-              borderRadius: BorderRadius.circular(widget.borderRadius!),
-            ),
-            child: Text(
-              widget.text,
-              style:
-                  GoogleFonts.plusJakartaSans(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                    color: AppColor.kWhite,
-                  ).copyWith(
-                    color: widget.textColor,
-                    fontWeight: FontWeight.w500,
-                    fontSize: widget.fontSize,
-                  ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-
