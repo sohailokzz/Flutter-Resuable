84015e0812defd14c3405fb77bdd27b401f95478
Muhammad Sohail <59048193+sohailokzz@users.noreply.github.com>
2025-10-30 22:19:39 +0500

    Secondary Button Added
    Secondary Button Added

--- lib/screens/auth_screens/login_screen.dart
+++ lib/screens/auth_screens/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_reusable/widgets/divider_row.dart';
 import 'package:flutter_reusable/widgets/primary_text_button.dart';
+import 'package:flutter_reusable/widgets/secondary_button.dart';
 import 'package:flutter_reusable/widgets/textfield_with_title.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
@@ -165,106 +166,6 @@ class LoginScreen extends StatelessWidget {
             ),
           );
         },
-      ),
-    );
-  }
-}
-
-class SecondaryButton extends StatefulWidget {
-  final VoidCallback onTap;
-  final String text, icons;
-  final double width;
-  final double height;
-  final double borderRadius;
-  final double? fontSize;
-  final Color textColor, bgColor;
-  const SecondaryButton({
-    super.key,
-    required this.onTap,
-    required this.text,
-    required this.width,
-    required this.height,
-    required this.icons,
-    required this.borderRadius,
-    this.fontSize,
-    required this.textColor,
-    required this.bgColor,
-  });
-
-  @override
-  State<SecondaryButton> createState() => _SecondaryButtonState();
-}
-
-class _SecondaryButtonState extends State<SecondaryButton>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  final Duration _animationDuration = const Duration(milliseconds: 300);
-  final Tween<double> _tween = Tween<double>(begin: 1.0, end: 0.95);
-  @override
-  void initState() {
-    _controller =
-        AnimationController(
-          vsync: this,
-          duration: _animationDuration,
-        )..addListener(() {
-          setState(() {});
-        });
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        _controller.forward().then((_) {
-          _controller.reverse();
-        });
-        widget.onTap();
-      },
-      child: ScaleTransition(
-        scale: _tween.animate(
-          CurvedAnimation(
-            parent: _controller,
-            curve: Curves.easeOut,
-            reverseCurve: Curves.easeIn,
-          ),
-        ),
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 17),
-          height: widget.height,
-          alignment: Alignment.center,
-          width: widget.width,
-          decoration: BoxDecoration(
-            color: widget.bgColor,
-            border: Border.all(color: AppColor.kLine),
-            borderRadius: BorderRadius.circular(widget.borderRadius),
-          ),
-          child: Row(
-            children: [
-              Image.asset(widget.icons, width: 23.85, height: 23.04),
-              const SizedBox(width: 12),
-              Text(
-                widget.text,
-                style:
-                    GoogleFonts.plusJakartaSans(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
-                      color: AppColor.kWhite,
-                    ).copyWith(
-                      color: widget.textColor,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w600,
-                    ),
-              ),
-            ],
-          ),
-        ),
       ),
     );
   }
